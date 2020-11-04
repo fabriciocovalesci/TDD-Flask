212508d77b4da11b864c613c9d55e90fea00b2ec
Fabricio Covalesci <fabcovalesci@gmail.com>
2020-11-04 02:23:14 -0300

    test ok, template_rendered

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
 
     @app.route('/login')
     def login():
-        return ''
+        return render_template('login.html')
 
     return app
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,5 @@
 from app import create_app
-from flask import Flask
+from flask import Flask, template_rendered
 
 def test_create_app():
     assert isinstance(create_app(), Flask)
@@ -20,6 +20,24 @@ def test_login_html():
     app.config['TESTING'] = True
 
     with app.test_client() as client:
-        response = client.get('/login')
+        request = client.get('/login')
 
     assert '<html>' in request.data.decode()
+
+def test_endpoint_de_login_retorna_template_login():
+    app = create_app()
+    app.config['TESTING'] = True
+
+    templates = []
+
+    def gravador_templates(remetente, template, context, **extra):
+        templates.append(template)
+
+    template_rendered.connect(gravador_templates, app)
+
+    with app.test_client() as client:
+        client.get('/login')
+
+    assert templates[0].name == 'login.html'
+
+    template_rendered.disconnect(gravador_templates, app)
